c5d5c939e7a0f6c909c9b33d4a96f744674a7ca1
hhp <624791164@qq.com>
2018-09-17 20:17:34 +0800

    练习

--- article-db.js
+++ article-db.js
@@ -1,12 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/article', { useNewUrlParser: true }, (err) => {
-    if (err) {
-        console.log('数据库链接失败');
-    } else {
-        console.log('连接成功');
-    }
-})
+mongoose.connect('mongodb://localhost:27017/article', { useNewUrlParser: true });
 
 const articleSchema = new mongoose.Schema({
     articleTitle: String,
