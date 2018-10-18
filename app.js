609b9bb7cc4317915b6f3923bc666aabb1541ef7
Hhpon <hhpone@163.com>
2018-10-18 12:33:16 +0800

    解决宽跨域问题

--- app.js
+++ app.js
@@ -7,6 +7,20 @@ const app = express();
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
+//allow custom header and CORS
+app.all('*',function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  
+    if (req.method == 'OPTIONS') {
+      res.send(200); /让options请求快速返回/
+    }
+    else {
+      next();
+    }
+  });
+
 app.post('/article', (req, res) => {
 
     Article.create({
