ebfe330749cc162afb2135e25e0d567ccb2f183c
Hhpon <624791164@qq.com>
2019-03-24 09:39:33 +0800

    test

--- app.js
+++ app.js
@@ -1,51 +1,59 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const Article = require('./article-db');
+const express = require("express");
+const bodyParser = require("body-parser");
+const Article = require("./article-db");
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(bodyParser.json());
 
 //allow custom header and CORS
-app.all('*', function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+app.all("*", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild"
+  );
+  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
 
-    if (req.method == 'OPTIONS') {
-        res.send(200); /让options请求快速返回/
-    }
-    else {
-        next();
-    }
+  if (req.method == "OPTIONS") {
+    res.send(200);
+    /让options请求快速返回/;
+  } else {
+    next();
+  }
 });
 
-app.post('/article', (req, res) => {
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
-    Article.create({
-        articleTitle: req.body.title,
-        articleAuthor: req.body.author,
-        articleContent: req.body.content
-    }, (err, doc) => {
-        if (err) {
-            res.end('no')
-        } else {
-            res.end('ok');
-        }
-    });
+app.post("/article", (req, res) => {
+  Article.create(
+    {
+      articleTitle: req.body.title,
+      articleAuthor: req.body.author,
+      articleContent: req.body.content
+    },
+    (err, doc) => {
+      if (err) {
+        res.end("no");
+      } else {
+        res.end("ok");
+      }
+    }
+  );
+});
 
-})
+app.get("/getArticle", (req, res) => {
+  Article.find((err, doc) => {
+    console.log(doc);
+    res.json(doc);
+  });
+});
 
-app.get('/getArticle', (req, res) => {
-    Article.find((err, doc) => {
-        console.log(doc);
-        res.json(doc)
-    });
-})
+app.get("/getNum", (req, res) => {
+  res.end("1");
+});
 
-app.get('/getNum', (req, res) => {
-    res.end('1');
-})
-
-app.listen(3000, () => console.log('服务器正在监听 3000 端口'));
+app.listen(3000, () => console.log("服务器正在监听 3000 端口"));
